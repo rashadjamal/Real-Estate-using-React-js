2b837e2c98e9edd303d7f5938003576590b053ae
Delt a rashadjamal <rashadjamalansari@gmail.com>
2024-02-18 18:52:17 +0530

    add more components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,11 @@ const Header = () => {
   return (
    
     <section className='h-wraqapper'>
-        <div className='flexCenter paddings innerWidth h-container'>
+        <div className='flexCenter paddings innerWidth  h-container'>
                 
                 <img src='./logo.png' alt='logo' width={100}/>
 
-         <div className=' flexCenter h-menu'>
+         <div className='flexCenter h-menu'>
             <a href=''>Residencies</a>
             <a href=''>Our Values</a>
             <a href=''>Contact Us</a>
